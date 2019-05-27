73b02bfc07da006953a5ec40da1a7b17c07be015
korir Cornelius kiprono <ceekayconelius@gmail.com>
2019-05-27 08:15:55 +0300

    Initial commit

---
+++ app/src/main/java/com/cornelius/memorableplacesrob/MapsActivity.java
@@ -0,0 +1,250 @@
+package com.cornelius.memorableplacesrob;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback , GoogleMap.OnMapLongClickListener {
+
+    private GoogleMap mMap;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length >0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+                locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER,0,0,locationListener);
+
+                Location lastKnown = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+
+                centerMaponLocation(lastKnown,"your location");
+
+            }
+
+        }
+
+    }
+
+    public void centerMaponLocation(Location location , String title){
+
+        LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+
+
+        if(title != "your location"){
+
+            mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,8));
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setOnMapLongClickListener(this);
+
+        Intent intent = getIntent();
+
+        if(intent.getIntExtra("placeNumber",0) == 0){
+
+            //zoom in on users location
+
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+
+            if (Build.VERSION.SDK_INT < 23){
+
+                locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER,0,0,locationListener);
+
+            }else{
+
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+
+                    locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER,0,0,locationListener);
+
+                    Location lastKnown = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+
+                   centerMaponLocation(lastKnown,"your location");
+
+                } else {
+
+                    ActivityCompat.requestPermissions(this ,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+                }
+
+
+            }
+
+
+        } else{
+
+            Location placeLocation = new Location(LocationManager.GPS_PROVIDER);
+            placeLocation.setLatitude(MainActivity.locations.get(intent.getIntExtra("placeNumber",0)).latitude);
+            placeLocation.setLongitude(MainActivity.locations.get(intent.getIntExtra("placeNumber",0)).longitude);
+
+
+            centerMaponLocation(placeLocation,MainActivity.places.get(intent.getIntExtra("placeNumber",0)));
+
+
+
+        }
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        String address = "";
+
+        try {
+            List<Address> listAddresses = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+
+            if (listAddresses != null && listAddresses.size() > 0){
+
+                if (listAddresses.get(0).getThoroughfare() !=null){
+
+                    if (listAddresses.get(0).getSubThoroughfare() != null ){
+
+                        address += listAddresses.get(0).getSubThoroughfare();
+
+                    }
+
+                    address += listAddresses.get(0).getThoroughfare();
+
+                }
+
+
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+         if (address == ""){
+
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm yyyy-MM-dd");
+            address = sdf.format(new Date());
+
+        }
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+
+
+         MainActivity.places.add(address);
+         MainActivity.locations.add(latLng);
+
+         MainActivity.arrayAdapter.notifyDataSetChanged();
+
+         SharedPreferences sharedPreferences = this.getSharedPreferences("com.cornelius.memorableplacesrob", Context.MODE_PRIVATE);
+
+        try {
+
+            ArrayList<String> latitudes = new ArrayList<>();
+            ArrayList<String> longitudes = new ArrayList<>();
+
+
+            for (LatLng coordinates : MainActivity.locations){
+
+                latitudes.add(Double.toString(coordinates.latitude));
+                longitudes.add(Double.toString(coordinates.longitude));
+
+            }
+
+
+            sharedPreferences.edit().putString("places",ObjectSerializer.serialize(MainActivity.places)).apply();
+            sharedPreferences.edit().putString("latitudes",ObjectSerializer.serialize(latitudes)).apply();
+            sharedPreferences.edit().putString("longitudes",ObjectSerializer.serialize(longitudes)).apply();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this, "location saved ", Toast.LENGTH_SHORT).show();
+
+    }
+}
